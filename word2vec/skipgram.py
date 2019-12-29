854a836440ebd45826f44214fabb33a6cdd468fe
Bijay Gaudel <gaudelbijay10@gmail.com>
2019-12-29 19:32:29 +0800

    create model

--- word2vec/skipgram.py
+++ word2vec/skipgram.py
@@ -1,12 +1,40 @@
 import numpy as np 
 import tensorflow as tf 
 from collections import defaultdict
+from tensorflow.keras.models import Model 
+
+def create_model(vector_size,embedding_size):
+    # Input Vectors 
+    X = tf.placeholder(tf.float32,shape=[None,self.vec_len])
+    Y = tf.placeholder(tf.float32,shape=[None,self.vec_len])
+
+    #Dictionary of Weights
+    weights = {
+            'w1': tf.Variable(tf.random_normal([self.vec_len,self.EMBEDDING_SIZE])),
+            'w2': tf.Variable(tf.random_normal([self.EMBEDDING_SIZE,self.vec_len])),
+        } 
+
+    #Dictionary of Biases
+    biases = {
+        'b1': tf.Variable(tf.random_normal([self.EMBEDDING_SIZE])),
+        'b2': tf.Variable(tf.random_normal([self.vec_len])),
+    }
+
+    # Forward pass 
+    hidden_1 = tf.add(tf.matmul(X,weights['w1']),biases['b1'])
+    out_layer = tf.add(tf.matmul(hidden_1,weights['w2']),biases['b2'])
+    softmax_out = tf.nn.softmax(out_layer)
+
+    model = Model(inputs=[X,Y],outputs=[out_layer])
+    emb = Model(inputs=[X],outputs=[softmax_out])
+    return model,emb 
 
 
 class word2vec:
-    def __init__(self,corpus,window_size):
+    def __init__(self,corpus,window_size,EMBEDDING_SIZE):
         self.corpus = corpus
         self.window_size = window_size
+        self.EMBEDDING_SIZE = EMBEDDING_SIZE
         word_ = set()
         for sentence in self.corpus:
             for word in sentence:
@@ -34,9 +62,3 @@ class word2vec:
         word_idx = self.word_idx[word]
         word_vec[word_idx]=1
         return word_vec
-
-    def create_model(self,):
-        
-
-
-
