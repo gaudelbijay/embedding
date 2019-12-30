4bbb5b4baa0e7058d25ada0c30ed43d7f9c96715
Bijay Gaudel <gaudelbijay10@gmail.com>
2019-12-30 15:28:38 +0800

    remove unused file

--- word2vec/main.py
+++ word2vec/main.py
@@ -4,7 +4,8 @@ from skipgram import word2vec
 def main():
     corpus = [['the','quick','brown','fox','jumped','over','the','lazy','dog']]
     obj = word2vec(corpus,window_size=3)
-    print(obj.train_model())
+    emb = obj.get_embeddings()
+    print(emb)
 
 if __name__ == '__main__':
     main()
